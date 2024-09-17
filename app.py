94575b6d173880f29bce889a4426825752e1ecea
Anessa Leng <ane.leng955@gmail.com>
2024-09-17 05:11:16 -0400

    Updating test files

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
-import mysql.connector
-from mysql.connector import Error
+#import mysql.connector
+#from mysql.connector import Error
 
 app = Flask(__name__)
 
